2e22296c53fe7a7bf041437c3c921cf50bbb7929
MehVahdJukaar <matteogozzi1998@gmail.com>
2023-05-03 08:13:06 +0200

    wip stuff

--- forge/src/main/java/net/mehvahdjukaar/jepp/forge/JeppForge.java
+++ forge/src/main/java/net/mehvahdjukaar/jepp/forge/JeppForge.java
@@ -41,56 +41,5 @@ public class JeppForge {
     }
 
 
-    public static final Capability<RamBreakingCap> RAM_BREAK_CAP = CapabilityManager.get(new CapabilityToken<>() {
-    });
-
-
-    @SubscribeEvent
-    public void registerCapabilities(RegisterCapabilitiesEvent event) {
-        event.register(RamBreakingCap.class);
-    }
-
-    @SubscribeEvent
-    public void attachCapabilities(AttachCapabilitiesEvent<Level> event) {
-        if (event.getObject() instanceof ServerLevel serverLevel) {
-            RamBreakingCap capability = new RamBreakingCap(serverLevel);
-            event.addCapability(Jepp.res("ram_break_progress"), capability);
-            event.addListener(capability::invalidate);
-        }
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void tickEvent(TickEvent.LevelTickEvent event) {
-        if (event.phase == TickEvent.Phase.END && event.level instanceof ServerLevel level) {
-            level.getCapability(RAM_BREAK_CAP).ifPresent(RamBreakingCap::validateAll);
-        }
-    }
-
-
-    @SubscribeEvent
-    public void onRemapBlocks(MissingMappingsEvent event) {
-        for (var v : event.getMappings(ForgeRegistries.BLOCKS.getRegistryKey(), "ohmygoat")) {
-            String name = v.getKey().getPath();
-            if (name.equals("ram_block")) {
-                v.remap(Jepp.RAM_BLOCK.get());
-            }
-        }
-        for (var v : event.getMappings(ForgeRegistries.ITEMS.getRegistryKey(), "ohmygoat")) {
-            String name = v.getKey().getPath();
-            switch (name) {
-                case "ram_block" -> v.remap(Jepp.RAM_BLOCK.get().asItem());
-                case "chevon" -> v.remap(Jepp.RAW_CHEVON.get().asItem());
-                case "cooked_chevon" -> v.remap(Jepp.COOKED_CHEVON.get().asItem());
-                case "barbaric_helmet" -> v.remap(Jepp.BARBARIC_HELMET.get().asItem());
-                case "geep_spawn_egg" -> v.remap(Jepp.GEEP_SPAWN_EGG.get().asItem());
-            }
-        }
-        for (var v : event.getMappings(ForgeRegistries.ENTITY_TYPES.getRegistryKey(), "ohmygoat")) {
-            String name = v.getKey().getPath();
-            if (name.equals("geep")) {
-                v.remap(Jepp.GEEP.get());
-            }
-        }
-    }
 }
 
